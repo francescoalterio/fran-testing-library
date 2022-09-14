78a838f57a7ad9217d14fbbe8f3d08bf0c41a6c2
Francesco Alterio <francescoalteriog@gmail.com>
2022-09-14 16:33:52 -0400

    library v1

--- __tests__/math.test.js
+++
@@ -1,5 +0,0 @@
-import { sum } from "../functions/math.js";
-
-test("5 + 10 = 15", () => {
-  expect(sum(5, 10)).toBe(15);
-});
--- functions/hello/hello.test.js
+++
@@ -1,5 +0,0 @@
-import { sayHello } from "./hello.js";
-
-test("say hello", () => {
-  expect(sayHello("hello")).toBe("hello");
-});
